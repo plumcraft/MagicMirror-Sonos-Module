775e04c81f220bd500630fc8f873c32daa258396
plumcraft <olivier.lebhard@gmail.com>
2017-01-29 21:41:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-{ module: 'mmm-sonos', position: 'top_right', config: { apiBase: 'http://ENV_SONOS_HTTP_API_ADDRESS', updateInterval: 0.3 }, classes: 'everyone' }
+{ module: 'mmm-sonos', position: 'top_right', config: { apiBase: 'http://ENV_SONOS_HTTP_API_ADDRESS', updateInterval: 0.3 }, classes: 'everyone' },
